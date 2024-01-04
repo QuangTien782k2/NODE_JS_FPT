47b36f20d5db7c746874c797c6181301a7c0521d
tienne <nguyenquangtien782002@gmail.com>
2024-01-04 10:51:26 +0700

    ver1

---
+++ index.js
@@ -0,0 +1,21 @@
+const ex = require('./rectangle')
+
+console.log(ex)
+// var rect = {
+//     chuvi : (x,y) => (2*(x+y)),
+//     dientich: (x,y) =>(x*y)
+// }
+
+// function slove (x, y){
+//     console.log("x: ", x, "\n y: ",y);
+//     if(x<= 0 || y <= 0){
+//         console.log('X, y must be greater than 0')
+//     }else
+//     {
+//         const cv =  rect.chuvi(x,y);
+//         const dt = rect.dientich(x,y);
+//         console.log('Chu vi :'+ cv +'\n Dien tich: '+ dt);
+//     }
+// }
+
+// slove(3,3);
---
+++ rectangle.js
@@ -0,0 +1,8 @@
+var Hello = 'rec hello world'
+
+var Hello2 = 'loloolooll'
+
+module.exports = [
+    Hello, 
+    Hello2
+]
